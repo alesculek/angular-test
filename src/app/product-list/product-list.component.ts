b8d3e3c1984100d633653afa39810cf007b9d6dd
alesculek <ales.culek@gmail.com>
2020-03-08 16:05:59 +0100

    4 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,7 +10,7 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
+  onShareBtnClick() {
     window.alert('The product has been shared!');
   }
 }
